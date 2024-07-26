95dd36cf017db55eda62fd2ee6882a1024e4dc27
ryuken73 <ryuken73@gmail.com>
2024-07-26 09:27:57 +0900

    update: use ref in useGSAP

--- src/renderer/Components/Player/DraggableTitle.js
+++ src/renderer/Components/Player/DraggableTitle.js
@@ -78,12 +78,10 @@ function DraggableTitle(props) {
     autoPlay,
   } = props;
   const progressRef = React.useRef(null);
-  console.log(isActive, autoPlay)
   useGSAP(
     () => {
       if (isActive && autoPlay) {
-        // gsap.to(progressRef.current, { width: '100%', duration: autoInterval });
-        gsap.to('.progress', { width: '100%', duration: autoInterval });
+        gsap.to(progressRef.current, { width: '100%', duration: autoInterval });
       }
     },
     {
@@ -102,7 +100,7 @@ function DraggableTitle(props) {
         </Live>
         <Banner titleFontSize={titleFontSize}>{title}</Banner>
         <Line>
-          <Progress className='progress' ref={progressRef} isActive={isActive} />
+          <Progress ref={progressRef} isActive={isActive} />
         </Line>
       </AbsoluteBox>
     </Draggable>
